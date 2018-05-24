db95a34872a46afcb9b2d1f8dba56069b4de4d8d
刘波 <1783851004@qq.com>
2018-05-24 17:20:23 +0800

    排行榜使用测试数据调试页面。签到页修改样式。

--- pages/entry/entry.js
+++ pages/entry/entry.js
@@ -1,7 +1,9 @@
 //index.js
 const app = getApp();
 
-function timing(that) {
+// 计时
+function timing(that) 
+{
   var seconds = that.data.seconds
   if (seconds > 3600) {
     that.setData({
@@ -26,7 +28,9 @@ function timing(that) {
     , 1000)
   formatSeconds(that)
 }
-function formatSeconds(that) {
+// 格式化秒
+function formatSeconds(that) 
+{
   var mins = 0, hours = 0, seconds = that.data.seconds, time = ''
   if (seconds < 60) {
 
@@ -43,66 +47,87 @@ function formatSeconds(that) {
     time: formatTime(hours) + ':' + formatTime(mins) + ':' + formatTime(seconds),
   });
 }
-function formatTime(num) {
+// 格式化时间
+function formatTime(num) 
+{
   if (num < 10)
     return '0' + num
   else
     return num + ''
 }
+// 关闭签到下面，便于重用
+function closeSignInPage(that) 
+{
+  var animation = wx.createAnimation({
+    duration: 500,
+    timingFunction: 'ease-in-out',
+  });
+  animation.opacity(0).scale3d(0,0,0).step({ duration: 1000 });
+  that.setData({
+    closeAnimation: animation,
+  })
+
+  setTimeout(function () {
+    that.setData({
+      signInToday: false
+    });
+  }, 1000);
+}
 
 Page({
 	data:
   {
+    // 是否已经获得用户信息
     hasUserInfo: false,
     seconds: 0,
+    // 计时显示
     time: '00:00:00',
+    // 用户获得的金币数
     coin: 0,
+    // 今天是否已经签到
     signInToday:true,
-    signInPreDays:[{"day":"第1天", "isSignIn":"true", "reward":"+30"},
+    // 签到记录初始化
+    signInPreDays:[{"day":"第1天", "isSignIn":"false", "reward":"+30"},
       { "day": "第2天", "isSignIn": "false", "reward": "+40" },
       { "day": "第3天", "isSignIn": "false", "reward": "+50" },
       { "day": "第4天", "isSignIn": "false", "reward": "+60" },
       { "day": "第5天", "isSignIn": "false", "reward": "+70" },
       { "day": "第6天", "isSignIn": "false", "reward": "+80" },
       { "day": "第7天", "isSignIn": "false", "reward": "+100" }],
+    // 7天签到的背景色
     colos: ["#b765e2", "#b765e2", "#b765e2", "#fe2380", "#8200bd", "#8200bd", "#8200bd"],
+    // 签到的动画数组
     animationCloudData: [{}, {}, {}, {}, {}, {}, {}, {}],
+    // 关闭签到页面的动画
+    closeAnimation:{},
 	},
 
 	onLoad(opt) 
   {
+    // 开始计时
     timing(this);
+    // 初始化是否已经登录
     this.setData({
       hasLogin: app.globalData.hasLogin
     })
+
 	},
 
   onReady()
   {
+    // 获取用户信息
     this.getUserMessage();
+    // 遍历签到状态，给需要签到的那一天加入动画
+    for (var i = 0; i < 7; i++)
+    {
+      if (this.data.signInPreDays[i]["isSignIn"] == "true")
+      {
+        this.addAnimationToDay(i);
+        break;
+      }
+    }
   },
-  tapDay0: function ()
-  {
-    this.addAnimationToDay(0);
-  },
-  tapDay1: function () {
-    this.addAnimationToDay(1);
-  },
-  tapDay2: function () {
-    this.addAnimationToDay(2);
-  },
-  tapDay3: function () {
-    this.addAnimationToDay(3);
-  },
-  tapDay4: function () {
-    this.addAnimationToDay(4);
-  },
-  tapDay5: function () {
-    this.addAnimationToDay(5);
-  },
-  tapDay6: function () {
-    this.addAnimationToDay(6);
-  },
+  // 给日期视图添加动画
   addAnimationToDay: function(index)
   {
     var that = this;
@@ -119,12 +144,12 @@ Page({
       })
     }, 500)
   },
-  
+  // 签到
   signIn: function()
   {
     this.closeSignInView();
   },
-
+  // 登录
   login: function () 
   {
     var that = this
@@ -139,13 +164,14 @@ Page({
       }
     })
   },
-
+  // 获取用户信息，绑定方法
   getUserInfo: function () 
   {
     this.getUserMessage();
   },
-
-  getUserMessage() {
+  // 获取用户信息，内部方法，方便重用
+  getUserMessage() 
+  {
     var that = this
 
     if (app.globalData.hasLogin === false) {
@@ -169,32 +195,13 @@ Page({
       })
     }
   },
-  
-  timeCount: function()
-  {
-
-  },
-
-	onShow() 
-  {
-    //当信道连接或者重连了时，关闭已连接的信道
-	//	this.closeTunnel()
-	},
-
+  // 前往游戏界面
 	gotoFighting() 
   {
 		wx.navigateTo({
       url: '../game/game_index'  
 		})
 	},
-
-	gotoFriends() 
-  {
-		wx.navigateTo({
-			url: '../friends_sort/friends_sort'
-		})
-	},
-
   //前往排行榜页面
   gotoLeaderboard: function()
   {
@@ -202,33 +209,19 @@ Page({
       url: '../rank/rank'
     })
   },
-
-	closeTunnel() 
+  // 用户点击右上角分享
+  onShareAppMessage: function () 
   {
-		//当信道连接或者重连了时，关闭已连接的信道
-		if (app.appData.tunnelStatus == 'connect' ||
-        app.appData.tunnelStatus == 'reconnect') 
-    {
-			app.tunnel.close();
-		}
-	},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
     return {
       title: 'IQ@高智商的你',
       path: '/pages/entry/entry',
       imageUrl: 'http://i0.hdslb.com/bfs/archive/2722a167806e5e732939ec3d847a9e9ac58e79e0.jpg'
     }
   },
-
+  // 关闭签到页面，绑定方法
   closeSignInView: function() 
   {
-    this.setData({
-      signInToday: false
-    });
+    closeSignInPage(this);
   }
 
 })
